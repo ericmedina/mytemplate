0ab0e5f2c7816f2209b3d60bef706ba090a93ebe
Eric Medina <eriic.eme@gmail.com>
2021-10-22 10:39:26 -0300

    Cambios install

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import Icon from './components/elements/Icon.vue';
-import Input from './components/elements/Input.vue';
-import Suggest from './components/elements/Suggest.vue';
-import TableList from './components/elements/TableList.vue';
-import FileSelect from './components/elements/FileSelect.vue';
-import Dropdown from './components/elements/Dropdown.vue';
-import Modal from './components/elements/Modal.vue';
+import Icon from '@/components/elements/Icon.vue';
+import Input from '@/components/elements/Input.vue';
+import Suggest from '@/components/elements/Suggest.vue';
+import TableList from '@/components/elements/TableList.vue';
+import FileSelect from '@/components/elements/FileSelect.vue';
+import Dropdown from '@/components/elements/Dropdown.vue';
+import Modal from '@/components/elements/Modal.vue';
 
 const PampaUI = {
-        install(Vue, options) {
+        install(Vue) {
             Vue.component('dropdown', Dropdown);
             Vue.component('icon', Icon);
             Vue.component('pui-input', Input);
